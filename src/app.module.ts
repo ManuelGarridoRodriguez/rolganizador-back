7dc6a380803c03bd7fbbef074ee5c32af26ce252
Cecrom <manu.garri@gmail.com>
2022-04-30 19:11:24 +0200

    Preparing and connecting mongo Atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://Cecrom:_R0ck!Rul3S_@cluster0.v69o7.mongodb.net/rolganizador?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
